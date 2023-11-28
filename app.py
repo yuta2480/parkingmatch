9312495b06d402f136d8ecd5295e5bc5ad368da8
yuta2480 <43869120+yuta2480@users.noreply.github.com>
2023-11-28 17:10:18 +0900

    20231128-2

--- app.py
+++ app.py
@@ -694,5 +694,5 @@ def adminDetailsAll():
 ## おまじない
 if __name__ == "__main__":
     # app.run(debug=True, host='192.168.1.111', port=1919)
-    app.run(debug=True, threaded=False)
+    app.run(debug=False, threaded=False)
     
